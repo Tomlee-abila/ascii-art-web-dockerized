650233e9d7d3a3daa86f786902e4d9e70fc89891
khahussein <kherld.hussein@gmail.com>
2024-08-22 08:58:45 +0300

    Feat(script): Implement automated build and run for docker file

---
+++ script.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Set the metadata labels
+LABEL_KEY="value"
+MAINTAINER="Doreen Onyango"
+MAINTAINER="Khalid Hussein"
+MAINTAINER="Tomlee Abila"
+VERSION="1.0"
+DESCRIPTION="This is a program that generates ASCII art based on a given string and banner style specified. The program uses a web interface that is appealing, intuitive and user friendly to displays the results.The application container built with Docker."
+
+# Build the Docker image
+docker build -t dockerize . \
+  --label "key=$LABEL_KEY" \
+  --label "maintainer=$MAINTAINER" \
+  --label "version=$VERSION" \
+  --label "description=$DESCRIPTION"
+
+# Check if the build was successful
+if [ $? -eq 0 ]; then
+  echo "Docker image built successfully."
+else
+  echo "Failed to build Docker image."
+  exit 1
+fi
+
+# Run the Docker container
+docker run -p 8080:8080 dockerize
